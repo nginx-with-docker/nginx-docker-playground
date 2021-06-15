107ae9e024ea529e6ec10436907a1b7228b796f1
soulteary <soulteary@gmail.com>
2021-06-15 11:12:31 +0800

    upd script

--- update.sh
+++ update.sh
@@ -18,6 +18,13 @@ echo $NGINX_VERSION:$NGINX_SHASUM
 echo "docker build --build-arg NGINX_VERSION=$NGINX_VERSION --build-arg NGINX_SHASUM=$NGINX_SHASUM --tag $DIST -f "$RELEASE_DIR/Dockerfile.alpine" ."
 docker build --build-arg NGINX_VERSION="$NGINX_VERSION" --build-arg NGINX_SHASUM="$NGINX_SHASUM" --tag $DIST -f "$RELEASE_DIR/Dockerfile.alpine" .
 
+if [[ "$(docker images -q $DIST 2> /dev/null)" != "" ]]; then
+    echo "Push: $DIST";
+    docker push $DIST;
+fi
+
+
+
 # Build For Debian
 
 TAG=base-$NGINX_VERSION;
@@ -28,3 +35,8 @@ echo "Build: $DIST";
 echo $NGINX_VERSION:$NGINX_SHASUM
 echo "docker build --build-arg NGINX_VERSION=$NGINX_VERSION --build-arg NGINX_SHASUM=$NGINX_SHASUM --tag $DIST -f "$RELEASE_DIR/Dockerfile.debian" ."
 docker build --build-arg NGINX_VERSION="$NGINX_VERSION" --build-arg NGINX_SHASUM="$NGINX_SHASUM" --tag $DIST -f "$RELEASE_DIR/Dockerfile.debian" .
+
+if [[ "$(docker images -q $DIST 2> /dev/null)" != "" ]]; then
+    echo "Push: $DIST";
+    docker push $DIST;
+fi
